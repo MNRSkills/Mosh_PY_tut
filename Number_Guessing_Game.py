a30090484ad32fce9978c949307d5eeda534548c
Mack Ramirez <mnrdev510@gmail.com>
2024-09-25 08:43:10 -0700

    Next is guessing game.

--- Number_Guessing_Game.py
+++ Number_Guessing_Game.py
@@ -2,4 +2,5 @@ def guess_a_number():
     """
         we need to ask an input to guess a number
         check to see if the number is too high or just right
+        the can gues number 1 through 100
     """
