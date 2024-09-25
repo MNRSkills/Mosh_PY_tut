4f26f9e4ae45ee8e9019bd531f7d6ff153e340ca
Mack Ramirez <mnrdev510@gmail.com>
2024-09-25 10:47:52 -0700

    This is the solution.

--- Number_Guessing_Game.py
+++ Number_Guessing_Game.py
@@ -1,6 +1,26 @@
+import random
+
 def guess_a_number():
     """
         we need to ask an input to guess a number
         check to see if the number is too high or just right
         the can gues number 1 through 100
     """
+
+    random_numb = random.randint(1, 100)
+    while True:
+        try:
+            random_choice = int(input("Guess number between 1 and 100: "))
+
+            if random_choice == random_numb:
+                print("this is the number")
+            elif random_choice > random_numb:
+                print("To low!")
+            elif random_choice < random_numb:
+                print("To high!")
+            else:
+                print("BING BING BING! Right on the money.")
+        except ValueError:
+            print("Please enter a valid number")
+
+guess_a_number()
