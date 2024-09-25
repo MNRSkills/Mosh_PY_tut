ff9d33c88c685f4ab46e762e74841ffeb72ae285
Mack Ramirez <mnrdev510@gmail.com>
2024-09-25 08:24:29 -0700

    Initial Commit

--- Roll_of_Dice.py
+++ Roll_of_Dice.py
@@ -1,11 +1,21 @@
+import random
+
+
 def dice_roll():
-   roll_die_or_not =  input("do you want to roll the die? (y/n): ").lower()
+
+   while True:
+      roll_die_or_not =  input("do you want to roll the die? (y/n): ").lower()
 
 
-   if roll_die_or_not == "y":
-      print("Maybe next time")
-   else:
-      print("dice is rolling")
+      if roll_die_or_not == "y":
+         first_die = random.randint(1, 6)
+         second_die = random.randint(1, 6)
+         print(f'({first_die}, {second_die})')
+      elif roll_die_or_not == "n":
+         print("Maybe next time")
+         break
+      else:
+         print('Invalid Choice')
 
 
 
