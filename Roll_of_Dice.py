702bf60196c2148a46e5e7e9fb6049ec1fbbb10f
Mack Ramirez <mnrdev510@gmail.com>
2024-09-25 07:23:31 -0700

    Initial Commit

---
+++ Roll_of_Dice.py
@@ -0,0 +1,13 @@
+def dice_roll():
+   roll_die_or_not =  input("do you want to roll the die? (y/n): ").lower()
+
+
+   if roll_die_or_not == "y":
+      print("Maybe next time")
+   else:
+      print("dice is rolling")
+
+
+
+
+dice_roll()
